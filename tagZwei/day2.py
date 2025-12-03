783b4a2e57cdbb2a9683c56ce4b02d821366cc05
Marc Helmreich <marc.helmreich@camlog.com>
2025-12-03 09:06:00 +0100

    Update Dateien und start Tag 3

--- tagZwei/day2.py
+++ tagZwei/day2.py
@@ -7,24 +7,50 @@ def process_and_handle_ranges(input):
     with open(input, 'r') as file:
         content = file.read().strip()
         ranges = content.split(',')
+        teile = None
 
         for range_str in ranges:
             number_range = range_to_array(range_str.strip())
             for number in number_range:
                 num_str = str(number)
                 num_length = len(num_str)
+                divisors = []
+                for i in range(1, number + 1):
+                    if number % i == 0:
+                        divisors.append(i)
+                for divisor in divisors:                                     
+                    if divisor != 1 and divisor != num_str:
+                        teile = number / divisor
+                        print("Teile: {teile} mit Divisor: {divisor} für Zahl: {number}")    
 
-                if num_length % 2 == 0:
-                    half_length = num_length // 2
-                    first_half = num_str[:half_length]
-                    second_half = num_str[half_length:]
 
-                    if first_half == second_half:
-                        old_solution = solution
-                        solution += number
-                        print("Neue Zahl gefunden: {number}")
-                        print("Rechnung: alte Lösung = {old_solution} + Zahl = {solution}")
 
     print("Die finale Lösung ist: {solution}")
 
 process_and_handle_ranges("input.txt")
+
+
+
+                        a = 1
+                        b = 0
+                        if a < teile:
+                            if a == 1:
+                                teil1 =  num_str[:divisor]
+                            if a == 2:
+                                teil2 =  num_str[divisor:divisor*2]   
+                            if a == 3:
+                                teil3 =  num_str[divisor*2:divisor*3]
+                            if a == 4:
+                                teil4 =  num_str[divisor*3:divisor*4]
+                            if a == 5:
+                                teil5 =  num_str[divisor*4:divisor*5]
+                            if a == 6:
+                                teil6 =  num_str[divisor*5:divisor*6]
+                            a += 1
+
+
+                                                    old_solution = solution
+                        solution += number
+                        print("Neue Zahl gefunden: {number}")
+                        print("Rechnung: alte Lösung = {old_solution} + Zahl = {solution}")
+                        break
