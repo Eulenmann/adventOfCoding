669045e85f4382d545842bb9d48f03a8eb23e41e
Marc Helmreich <marc.helmreich@camlog.com>
2025-12-11 11:58:13 +0100

    Tage Angelegt und Tag 2 Aufgabe 1 korrigiert

--- tagZwei/day2_1.py
+++ tagZwei/day2_1.py
@@ -1,9 +1,9 @@
+solution = 0
 def range_to_array(range_str):
     start, end = map(int, range_str.split('-'))
     return range(start, end + 1)
 
 def process_and_handle_ranges(filename):
-    solution = 0
     with open(filename, 'r') as file:
         content = file.read().strip()
         ranges = content.split(',')
@@ -13,21 +13,21 @@ def process_and_handle_ranges(filename):
             for number in number_range:
                 num_str = str(number)
                 num_length = len(num_str)
-                check_if_even_and_left_equals_right(num_length,num_str)
+                check_if_even_and_left_equals_right(num_length,num_str,number)
+    return solution
 
-def check_if_even_and_left_equals_right(num_length,num_str):
-                if num_length % 2 == 0:
-                    half_length = num_length // 2
-                    first_half = num_str[:half_length]
-                    second_half = num_str[half_length:]
-
-                    if first_half == second_half:
-                        old_solution = solution
-                        solution += number
-                        print(f"Neue Zahl gefunden: {number}")
-                        print(f"Rechnung: alte Lösung = {old_solution} + Zahl = {solution}")
-
-    print(f"Die finale Lösung ist: {solution}")
+def check_if_even_and_left_equals_right(num_length,num_str,number):
+    global solution
+    if num_length % 2 == 0:
+        half_length = num_length // 2
+        first_half = num_str[:half_length]
+        second_half = num_str[half_length:]
+        if first_half == second_half:
+            old_solution = solution
+            solution += number
+            #print(f"Neue Zahl gefunden: {number}")
+            #print(f"Rechnung: alte Lösung = {old_solution} + Zahl = {solution}")
 
 # Beispielaufruf
-process_and_handle_ranges("input.txt")
+final_solution = process_and_handle_ranges("input.txt")
+print(f"Die finale Lösung ist: {final_solution}")
